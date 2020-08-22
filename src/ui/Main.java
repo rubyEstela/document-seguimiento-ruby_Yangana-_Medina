35e9d4a2914e7eb3a219dde8cc04a3b18d902691
rubyEstela <ymedina.ruby@gmail.com>
2020-08-21 20:34:59 -0500

    comit dos

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,15 +7,21 @@ import model.Client;
 import model.MiniMarket;
 
 
+
 public class Main {
 	private static MiniMarket minimarket;
+	
 	public static  void menu() {
-		Client cl = new Client(0,0 );
+		
 		Scanner sc = new Scanner(System.in);
+		Client cl = new Client( 0,"");
 		Date date=new Date();
 		System.out.println(" \n              \n       ***********BIENVENIDOS********* *    \n ");
 		System.out.println("--  MI BARRIO TE QUIERE --");
-		System.out.println("INGRESE  EL NUMERO QUE CORRESPONDA A SU TIPO DE DOCUMENTO  \n 1)TI  \n 2)CC \n 3)PP \n 4 CE");
+		System.out.println("INGRESE  EL NUMERO QUE CORRESPONDA A SU TIPO DE DOCUMENTO  \n 1)TI  \n 2)CC \n 3)PP \n 4 CE"+cl.typeDocuments());
+		int types= sc.nextInt();
+		System.out.println("por favor digite su numero de documento de identidad");
+		int num=sc.nextInt();
 		
 		
 		
@@ -23,6 +29,12 @@ public class Main {
 	}
 	 
 	
+		  
+		
+	
+		
+		
+		
 	
 
 
